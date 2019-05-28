eee9742d0699f7d849e39bbcf31aedc255a498f6
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-05-28 11:32:28 -0400

    Update HomeActivity.java

--- main/java/com/owen/netmall/HomeActivity.java
+++ main/java/com/owen/netmall/HomeActivity.java
@@ -11,6 +11,8 @@ import io.paperdb.Paper;
 public class HomeActivity extends AppCompatActivity {
 
     private Button logoutbtn;
+    private Button Loginbtn;
+    private TextView tvAlreadySignedUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +20,14 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         logoutbtn = (Button)findViewById(R.id.btnLogout);
+        Loginbtn = (Button) findViewById(R.id.btnLogin);
 
         logoutbtn.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v) {
-
+                
+                //used to make sure a user once loged in is remembeered
                 Paper.book().destroy();
 
                 Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
